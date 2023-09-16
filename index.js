de94d31c1695b90b1155edd9d4233fe1e066010f
sooya <ghgfhfg2@naer.com>
2023-09-16 19:32:09 +0900

    .

--- index.js
+++ index.js
@@ -11,29 +11,44 @@ app.use(
   })
 );
 
-request(url, (error, response, body) => {
-  if (error) throw error;
-  let $ = cheerio.load(body);
-  try {
-    var sum = 0;
-    var size = 0;
-    $("tbody tr td:nth-child(6)").each(function (index, elem) {
-      var rate_text = $(this).text(); //해당 태그의 text부분만 잘라오기
-      var filtered_rate_text = rate_text.substring(0, rate_text.length - 1); //'%'자르기
-      var rate = parseFloat(filtered_rate_text); //문자열 형태의 정답률을 float형으로 변환
-      console.log(rate);
-      sum += rate;
-      size += $(this).length;
-    });
-    console.log("Size=" + size);
-    console.log("Average Correct Percentage=" + (sum / size).toFixed(3) + "%");
-  } catch (error) {
-    console.error(error);
-  }
+let scrapingResult = {
+  date: "",
+  the_basic_rate: "",
+  buy: "",
+  sell: "",
+};
+const getData = () => {
+  request(
+    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blUw&qvt=0&query=%EC%A3%BC%EA%B0%84%EB%93%9C%EB%9D%BC%EB%A7%88%20%EC%8B%9C%EC%B2%AD%EB%A5%A0",
+    (err, res, body) => {
+      const $ = cheerio.load(body);
+      const list = $(".scroll_bx > .tb_list tbody tr").map((i, el) => {
+        const text = $(el).find("td:nth-child(2) p a").text();
+        return text;
+      });
+
+      console.log(list);
+    }
+  );
+};
+//getData();
+app.get("/getMessage", async (req, res) => {
+  request(
+    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blUw&qvt=0&query=%EC%A3%BC%EA%B0%84%EB%93%9C%EB%9D%BC%EB%A7%88%20%EC%8B%9C%EC%B2%AD%EB%A5%A0",
+    (err, res, body) => {
+      const $ = cheerio.load(body);
+      const list = $(".scroll_bx > .tb_list tbody tr").map((i, el) => {
+        const text = $(el).find("td:nth-child(2) p a").text();
+        return text;
+      });
+      res.json({
+        test: "1",
+        list,
+      });
+    }
+  );
 });
 
-app.get("/getMessage", async (req, res) => {});
-
-const server = app.listen(process.env.PORT || "3000", () => {
+const server = app.listen(process.env.PORT || "4000", () => {
   console.log("server listening on port %s", server.address().port);
 });
