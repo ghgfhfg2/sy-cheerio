ce78075d9abf2c80dd71f35b08f4ffc26daaa213
sooya <ghgfhfg2@naer.com>
2023-09-17 09:32:07 +0900

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const request = require("request");
 const cheerio = require("cheerio");
+const axios = require("axios");
 
 const app = express();
 app.use(
@@ -17,36 +17,32 @@ let scrapingResult = {
   buy: "",
   sell: "",
 };
-const getData = () => {
-  request(
-    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blUw&qvt=0&query=%EC%A3%BC%EA%B0%84%EB%93%9C%EB%9D%BC%EB%A7%88%20%EC%8B%9C%EC%B2%AD%EB%A5%A0",
-    (err, res, body) => {
-      const $ = cheerio.load(body);
-      const list = $(".scroll_bx > .tb_list tbody tr").map((i, el) => {
-        const text = $(el).find("td:nth-child(2) p a").text();
-        return text;
-      });
 
-      console.log(list);
-    }
-  );
-};
-//getData();
-app.get("/getMessage", async (req, res) => {
-  request(
-    "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blUw&qvt=0&query=%EC%A3%BC%EA%B0%84%EB%93%9C%EB%9D%BC%EB%A7%88%20%EC%8B%9C%EC%B2%AD%EB%A5%A0",
-    (err, res, body) => {
-      const $ = cheerio.load(body);
-      const list = $(".scroll_bx > .tb_list tbody tr").map((i, el) => {
-        const text = $(el).find("td:nth-child(2) p a").text();
-        return text;
+app.get("/getRank", async (req, res) => {
+  axios
+    .get(
+      "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blUw&qvt=0&query=%EC%A3%BC%EA%B0%84%EB%93%9C%EB%9D%BC%EB%A7%88%20%EC%8B%9C%EC%B2%AD%EB%A5%A0"
+    )
+    .then((res2) => {
+      const $ = cheerio.load(res2.data);
+      let arr = [];
+      const list = $(".scroll_bx > .tb_list tbody tr");
+      list.each((i, el) => {
+        const rank = $(el).find("td:nth-child(1) p .blind").text();
+        const title = $(el).find("td:nth-child(2) p a").text();
+        const ch = $(el).find("td:nth-child(3) p a").text();
+        arr.push({
+          rank,
+          title,
+          ch,
+        });
       });
+      console.log(arr);
       res.json({
-        test: "1",
-        list,
+        tset: "test",
+        test2: JSON.stringify(arr),
       });
-    }
-  );
+    });
 });
 
 app.get("/", (req, res) => {
